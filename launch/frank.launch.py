6cff1406b9626be8d7de068085b8082dc062656c
Martin Bauernschmitt <martin.bauernschmitt@francor.de>
2022-05-11 12:15:45 +0200

    Changed default timeout

--- launch/frank.launch.py
+++ launch/frank.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
                 {
                     "can": "can0",
                     "cmd_vel_max_timeout_s" : 1.0,
-                    "enable_cmd_vel_timeout" : False,
+                    "enable_cmd_vel_timeout" : True,
                     "auto_enable_on_start": False,
                     "auto_enable": True,
                     "accel_limit": 25.0,
